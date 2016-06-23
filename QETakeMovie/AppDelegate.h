a2ca7e23ddb1af2a46749c77d8f51d35657bc556
mobin <mobin-888@163.com>
2016-06-24 00:21:36 +0800

    Initial Commit

---
+++ QETakeMovie/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QETakeMovie
+//
+//  Created by user on 16/6/24.
+//  Copyright © 2016年 mobin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QETakeMovie/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QETakeMovie
+//
+//  Created by user on 16/6/24.
+//  Copyright © 2016年 mobin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
